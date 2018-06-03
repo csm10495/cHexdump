ca31f6c72b2a54b02257198fa61813c788b2b94e
csm10495 <csm10495@gmail.com>
2018-06-03 14:28:30 -0700

    Fix packaging?

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='csm10495',
     author_email='csm10495@gmail.com',
     url='http://github.com/csm10495/chexdump',
-    version='0.1',
+    version='0.2',
     packages=['chexdump'],
     license='MIT License',
     python_requires='>=2.7',
@@ -25,4 +25,5 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+    include_package_data = True,
 )
