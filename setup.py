fedfa5b86f9e3ab9bfb6bb97d9903e81fe687ed8
csm10495 <csm10495@gmail.com>
2018-06-03 14:32:56 -0700

    upversion and should be done for now

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='csm10495',
     author_email='csm10495@gmail.com',
     url='http://github.com/csm10495/chexdump',
-    version='0.2',
+    version='0.3',
     packages=['chexdump'],
     license='MIT License',
     python_requires='>=2.7',
